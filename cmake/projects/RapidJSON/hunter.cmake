95affe0d1200649d204ed150a0a97238d08ee699
Rahul Sheth <rahul@snap.com>
2020-08-27 16:28:11 -0400

    Update RapidJSON to v1.1.0-ce81bc9-p1 (#252)

--- cmake/projects/RapidJSON/hunter.cmake
+++ cmake/projects/RapidJSON/hunter.cmake
@@ -13,11 +13,11 @@ hunter_add_version(
     PACKAGE_NAME
     RapidJSON
     VERSION
-    "1.1.0-ce81bc9-p0"
+    "1.1.0-ce81bc9-p1"
     URL
-    "https://github.com/cpp-pm/rapidjson/archive/v1.1.0-ce81bc9-p0.tar.gz"
+    "https://github.com/cpp-pm/rapidjson/archive/v1.1.0-ce81bc9-p1.tar.gz"
     SHA1
-    95b85cd29fea30d4cbd1478bcc24a06ff60b79f8
+    2fb99e1c64dcd6702a56efb1bea9341c67c1d6e3
 )
 
 hunter_add_version(
